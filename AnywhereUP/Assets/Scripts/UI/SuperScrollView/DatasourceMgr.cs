b890cd2bf7e0728ff84c6b88c471144585612c30
Jeno <lishujie0571@163.com>
2018-04-14 22:06:30 +0800

    UI:infinite scrollview

---
+++ AnywhereUP/Assets/Scripts/UI/SuperScrollView/DatasourceMgr.cs
@@ -0,0 +1,122 @@
+﻿/*
+*	Function:
+*		
+*		
+*	Author:
+*		Jeno
+*		
+*/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Anywhere.UI
+{
+
+    public class ItemData
+    {
+        public int mId;//Data索引
+        public string mSpriteName;//图片名称
+        public string mLocation;//位置
+        public string mDes;//描述
+    }
+
+    public class DatasourceMgr : MonoBehaviour
+    {
+
+        List<ItemData> mItemDataList = new List<ItemData>();
+        static DatasourceMgr instance = null;
+
+
+        public static DatasourceMgr Get
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = Object.FindObjectOfType<DatasourceMgr>();
+                }
+                return instance;
+            }
+        }
+
+        void Awake()
+        {
+            Init();
+        }
+
+        public void Init()
+        {
+            DoRefreshDataSource();
+        }
+
+        /// <summary>
+        /// 根据item索引取item
+        /// </summary>
+        /// <param name="index"></param>
+        /// <returns></returns>
+        public ItemData GetItemDataByIndex(int index)
+        {
+            if (index < 0 || index >= mItemDataList.Count)
+            {
+                Debug.LogError("can't GetItemDataByIndex index is:" + index);
+                return null;
+            }
+            return mItemDataList[index];
+        }
+
+        /// <summary>
+        /// 根据ItemData的Id取item
+        /// </summary>
+        /// <param name="itemId"></param>
+        /// <returns></returns>
+        public ItemData GetItemDataById(int itemId)
+        {
+            int count = mItemDataList.Count;
+            for (int i = 0; i < count; ++i)
+            {
+                if (mItemDataList[i].mId == itemId)
+                {
+                    return mItemDataList[i];
+                }
+            }
+            return null;
+        }
+
+        
+        public int TotalItemCount
+        {
+            get
+            {
+                return mItemDataList.Count;
+            }
+        }
+
+        //public void SetDataTotalCount(int count)
+        //{
+        //    mTotalDataCount = count;
+        //    DoRefreshDataSource();
+        //}
+
+
+        int DataCount = 10;//数据条数 （测试）
+        /// <summary>
+       /// 获取数据(测试）
+       /// </summary>
+        void DoRefreshDataSource()
+        {
+            mItemDataList.Clear();
+            for (int i = 0; i < DataCount; i++)
+            {
+                ItemData itemData = new ItemData();
+                itemData.mId = i;
+                itemData.mDes = "这是描述：随机数" + Random.Range(0, 20);
+                itemData.mLocation = "位置：" + Random.Range(0, 20);
+                mItemDataList.Add(itemData);
+            }
+        }
+
+    }
+
+}
---
+++ AnywhereUP/Assets/Scripts/UI/SuperScrollView/ListItem.cs
@@ -0,0 +1,51 @@
+﻿/*
+*	Function:
+*		列表单元初始化、属性设置
+*		
+*	Author:
+*		Jeno
+*		
+*/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Anywhere.UI
+{
+
+    public class ListItem : MonoBehaviour
+    {
+
+        public GameObject mContentRootObj;
+
+        public Text mIndexText;//索引
+        public Text mDesText;//描述
+        public Text mLoactionText;//位置
+        public Image mIcon;//图片
+
+        public Button mDownloadBtn;//下载按钮
+
+        public void Init()
+        {
+            ClickEventListener listener = ClickEventListener.Get(mDownloadBtn.gameObject);
+            listener.SetClickEventHandler(OnDownloadBtnClick);
+        }
+
+        public void SetItemData(ItemData itemData, int itemIndex)
+        {
+
+            mIndexText.text = itemIndex.ToString();
+            mDesText.text = itemData== null ? " " : itemData.mDes;
+            mLoactionText.text = itemData== null ? " " : itemData.mLocation;
+
+        }
+
+        void OnDownloadBtnClick(GameObject btn)
+        {
+            Debug.Log("开始下载");
+        }
+
+    }
+}
