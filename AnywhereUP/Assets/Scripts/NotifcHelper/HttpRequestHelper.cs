389a07f693fc96ca02ffbc389e635c94b09eee6b
NSWell <paris3@163.com>
2018-05-07 22:22:58 +0800

    update httprequestHelper

--- AnywhereUP/Assets/Scripts/NotifcHelper/HttpRequestHelper.cs
+++ AnywhereUP/Assets/Scripts/NotifcHelper/HttpRequestHelper.cs
@@ -6,6 +6,8 @@ public class HttpRequestHelper : EventArgs
 {
     public string m_URI;
     public int m_TimeOut;
+    public string m_LocalPath;
     public Action<string> m_Succeed;
+    public Action<float> m_Downloading;
     public Action m_Failed;
 }
