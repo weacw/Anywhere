590c8598e035e82ebfa4451ddfb413b53a37eac6
NSWell <paris3@163.com>
2018-05-21 22:36:02 +0800

    Code comments HttpDownload/Request

--- AnywhereUP/Assets/Scripts/AppBaseModules/HttpDownload.cs
+++ AnywhereUP/Assets/Scripts/AppBaseModules/HttpDownload.cs
@@ -1,26 +1,32 @@
-﻿using UnityEngine;
-using System.IO;
-using System.Net;
-using System.Security.Cryptography.X509Certificates;
-using System.Net.Security;
-using System.Threading;
+﻿//用于多线程http文件下载
 
 namespace Anywhere
 {
+    using UnityEngine;
+    using System.IO;
+    using System.Net;
+    using System.Security.Cryptography.X509Certificates;
+    using System.Net.Security;
+
     [CreateAssetMenu(menuName = "Anywhere/AppModules/Http/Http Download")]
     public class HttpDownload : BaseModule
     {
+        //线程锁-锁头，可以保证当有线程操作某个共享资源时，其他线程必须等待直到当前线程完成操作
+        private object m_LockObject;
 
-        private object obj;
         public void HttpDownloadFile(Notification _notif)
         {
-            if (obj == null) obj = new object();
-            lock (obj)
+            if (m_LockObject == null) m_LockObject = new object();
+            lock (m_LockObject)
             {
+                //获取NoticeCenter发来的消息参数
                 HttpRequestHelper tmp_httpRequestHelper = _notif.param as HttpRequestHelper;
+
+                //证书
                 ServicePointManager.ServerCertificateValidationCallback = delegate (object s, X509Certificate certificate, X509Chain chain, SslPolicyErrors sslPolicyErrors) { return true; };
                 string tmpPath = Path.GetDirectoryName(tmp_httpRequestHelper.m_LocalPath);
 
+                //创建文件夹
                 if (!Directory.Exists(tmpPath))
                     Directory.CreateDirectory(tmpPath);
 
@@ -28,18 +34,22 @@ namespace Anywhere
                 {
                     string tmpFile = Path.Combine(tmpPath, Path.GetFileName(tmp_httpRequestHelper.m_URI));
 
-                    //本地文件不存在则从网上下载
+                    //检查缓存,缓存不存在则从网上下载
                     if (!CacheMachine.IsCache(tmpFile))
                     {
+                        //正式发起http请求
                         HttpWebRequest tmpRequest = WebRequest.Create(tmp_httpRequestHelper.m_URI) as HttpWebRequest;
                         tmpRequest.Timeout = tmp_httpRequestHelper.m_TimeOut;
                         HttpWebResponse tmpResponse = tmpRequest.GetResponse() as HttpWebResponse;
                         long tmpTotalBytes = tmpResponse.ContentLength;
 
+                        //获取服务器返回的数据，并写入本地指定目录(tmpFile目录)
                         Stream tmpResponseStream = tmpResponse.GetResponseStream();
                         byte[] tmpBytes = new byte[1024];
                         int size = tmpResponseStream.Read(tmpBytes, 0, (int)tmpBytes.Length);
                         FileStream tmpFileStream = new FileStream(tmpFile, FileMode.Append, FileAccess.Write, FileShare.ReadWrite);
+
+                        //计算http下载文件的进度
                         long tmpCurTotalBytes = 0;
                         while (size > 0)
                         {
@@ -47,37 +57,38 @@ namespace Anywhere
                             tmpFileStream.Write(tmpBytes, 0, size);
                             size = tmpResponseStream.Read(tmpBytes, 0, (int)tmpBytes.Length);
                             float progress = (float)tmpCurTotalBytes / (float)tmpTotalBytes;
+
+                            //下载中的回调
                             if (tmp_httpRequestHelper.m_Downloading != null)
                             {
-                                Loom.QueueOnMainThread((parm) =>
-                                {
-                                    tmp_httpRequestHelper.m_Downloading.Invoke(progress);
-                                }, null);
+                                //划入主线程的回调
+                                Loom.QueueOnMainThread((parm) => { tmp_httpRequestHelper.m_Downloading.Invoke(progress); }, null);
                             }
                         }
                         tmpFileStream.Close();
                         tmpResponseStream.Close();
                     }
 
+                    //下载完毕的回调
                     if (tmp_httpRequestHelper.m_Succeed != null)
                     {
-                        Loom.QueueOnMainThread((parm) =>
-                        {
-                            tmp_httpRequestHelper.m_Succeed.Invoke(null);
-                        }, null);
+                        Loom.QueueOnMainThread((parm) => { tmp_httpRequestHelper.m_Succeed.Invoke(null); }, null);
                     }
                 }
                 catch (WebException ex)
                 {
+                    //下载出错
                     if (tmp_httpRequestHelper.m_Failed != null)
                         tmp_httpRequestHelper.m_Failed.Invoke();
                     Debug.LogError(ex.Message);
                 }
             }
         }
+
         private void OnDisable()
         {
-            obj = null;
+            //由于script object table 是可存储的，一旦设置变量后就会被保存下来。固在disable后把线程锁清空
+            m_LockObject = null;
         }
     }
 }
--- AnywhereUP/Assets/Scripts/AppBaseModules/HttpRequest.cs
+++ AnywhereUP/Assets/Scripts/AppBaseModules/HttpRequest.cs
@@ -1,52 +1,67 @@
-﻿using System.IO;
-using System.Net;
-using System;
-using UnityEngine;
-using System.Security.Cryptography.X509Certificates;
-using System.Net.Security;
-using System.Threading;
+﻿//用于向服务器发起http-get请求模块
 namespace Anywhere
 {
+    using System.IO;
+    using System.Net;
+    using UnityEngine;
+    using System.Security.Cryptography.X509Certificates;
+    using System.Net.Security;
+    using System.Threading;
+
     [CreateAssetMenu(menuName = "Anywhere/AppModules/Http/Http Requset")]
     public class HttpRequest : BaseModule
     {
         public void GetHttpResponse(Anywhere.Notification _notif)
         {
-
+            //证书验证
             ServicePointManager.ServerCertificateValidationCallback = delegate (object s, X509Certificate certificate, X509Chain chain, SslPolicyErrors sslPolicyErrors) { return true; };
+
+            //获取NoticeCenter发送过来的消息参数
             HttpRequestHelper tmp_RequestHelper = _notif.param as HttpRequestHelper;
 
-            Loom.RunAsync(() =>
-            new Thread(() =>
+            //划出另外一个线程
+            Loom.RunAsync(() => new Thread(() =>
             {
                 try
                 {
+                    //开始向服务器发送请求的回调
                     if (tmp_RequestHelper.m_Starting != null) tmp_RequestHelper.m_Starting.Invoke();
+
+                    //正式向服务器发起http-get请求
                     HttpWebRequest request = WebRequest.Create(tmp_RequestHelper.m_URI) as HttpWebRequest;
                     request.Method = "GET";
                     if (tmp_RequestHelper.m_TimeOut != 0)
                         request.Timeout = tmp_RequestHelper.m_TimeOut;
                     HttpWebResponse response = request.GetResponse() as HttpWebResponse;
+
+                    //获取服务器反馈回来的消息
                     StreamReader tmp_Reader = new StreamReader(response.GetResponseStream(), System.Text.Encoding.UTF8);
+
+                    //由于不是主线程无法执行UnityEngine-API，所以需要将当前线程划入主线程
                     Loom.QueueOnMainThread((parm) =>
                     {
+                        //执行请求成功后的回调
                         if (tmp_RequestHelper.m_Succeed != null)
                             tmp_RequestHelper.m_Succeed.Invoke(tmp_Reader.ReadToEnd());
                     }, null);
                 }
                 catch (WebException ex)
                 {
+                    //请求错误
                     Debug.LogError(ex.Message);
                     switch ((ex.Response as HttpWebResponse).StatusCode)
                     {
                         case HttpStatusCode.NotFound:
                         case HttpStatusCode.GatewayTimeout:
-                            if (tmp_RequestHelper.m_Failed != null)
-                                tmp_RequestHelper.m_Failed.Invoke();
+                            Loom.QueueOnMainThread((parm) =>
+                            {
+                                if (tmp_RequestHelper.m_Failed != null)
+                                    tmp_RequestHelper.m_Failed.Invoke();
+                            }, null);
                             break;
                     }
                 }
-            }).Start());         
+            }).Start());
         }
     }
 }
